4226a0ee8df1d78c31e24cdb4674e5d3334e6a3f
AlexJWayne <alex.wayne@solvenergy.com>
2025-03-18 22:37:28 -0700

    Update keymap.c

--- keyboards/bastardkb/skeletyl/keymaps/colemakdh/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/colemakdh/keymap.c
@@ -19,12 +19,11 @@
 
 enum custom_keycodes {
     JSFN = SAFE_RANGE,
-    BBB,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case BBB:
+    case JSFN:
         if (record->event.pressed) {
             SEND_STRING("=>");
         }
@@ -52,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
      KC_PERC, KC_MINS, KC_CIRC, KC_DLR,  XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_GRV,  KC_AMPR, KC_EQL,  KC_COLN, BBB,                                   XXXXXXX, KC_RSFT, KC_RGUI, KC_RALT, KC_RCTL,
+     KC_GRV,  KC_AMPR, KC_EQL,  KC_COLN, JSFN,                                XXXXXXX, KC_RSFT, KC_RGUI, KC_RALT, KC_RCTL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_EXLM, KC_PIPE, KC_AT,   KC_HASH, KC_BSLS,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
